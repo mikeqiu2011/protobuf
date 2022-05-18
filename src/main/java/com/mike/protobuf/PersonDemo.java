2e0b3eb43152a23ea3cb08790532584b63e49c0d
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-18 09:15:28 +0800

    serialize Person obj

--- src/main/java/com/mike/protobuf/PersonDemo.java
+++ src/main/java/com/mike/protobuf/PersonDemo.java
@@ -2,18 +2,19 @@ package com.mike.protobuf;
 
 import com.mike.models.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo {
-    public static void main(String[] args) {
-        Person sam1 = Person.newBuilder()
+    public static void main(String[] args) throws IOException {
+        Person sam = Person.newBuilder()
                 .setName("sam")
                 .setAge(10)
                 .build();
 
-        Person sam2 = Person.newBuilder()
-                .setName("sam")
-                .setAge(10)
-                .build();
-
-        System.out.println(sam1.equals(sam2));
+        Path path = Paths.get("sam.ser");
+        Files.write(path, sam.toByteArray());
     }
 }
