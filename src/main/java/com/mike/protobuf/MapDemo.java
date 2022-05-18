52a3c5cfb9d2467e15a531e5c9b1119801b7442e
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-18 11:37:58 +0800

    map demo

---
+++ src/main/java/com/mike/protobuf/MapDemo.java
@@ -0,0 +1,26 @@
+package com.mike.protobuf;
+
+import com.mike.models.Car;
+import com.mike.models.Dealer;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        Car accord = Car.newBuilder().setMake("honda")
+                .setModel("accord")
+                .setYear(2020)
+                .build();
+
+        Car civic = Car.newBuilder().setMake("honda")
+                .setModel("Civic")
+                .setYear(2005)
+                .build();
+
+        Dealer dealer = Dealer.newBuilder().putModel(2020, accord)
+                .putModel(2005, civic).build();
+
+        System.out.println(
+//                dealer.getModelOrThrow(2006)
+                dealer.getModelOrDefault(2006, accord)
+        );
+    }
+}
