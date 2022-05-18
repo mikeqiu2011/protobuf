05f7398aeb8889278a39a7ca329bff9850e265ef
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-18 11:54:34 +0800

    set bodyStyle enum

--- src/main/java/com/mike/protobuf/MapDemo.java
+++ src/main/java/com/mike/protobuf/MapDemo.java
@@ -1,5 +1,6 @@
 package com.mike.protobuf;
 
+import com.mike.models.BodyStyle;
 import com.mike.models.Car;
 import com.mike.models.Dealer;
 
@@ -8,11 +9,13 @@ public class MapDemo {
         Car accord = Car.newBuilder().setMake("honda")
                 .setModel("accord")
                 .setYear(2020)
+                .setBodyStyle(BodyStyle.SEDAN)
                 .build();
 
         Car civic = Car.newBuilder().setMake("honda")
                 .setModel("Civic")
                 .setYear(2005)
+                .setBodyStyle(BodyStyle.COUPE)
                 .build();
 
         Dealer dealer = Dealer.newBuilder().putModel(2020, accord)
@@ -20,7 +23,7 @@ public class MapDemo {
 
         System.out.println(
 //                dealer.getModelOrThrow(2006)
-                dealer.getModelOrThrow(2005).getBodyStyle()
+                dealer.getModelOrThrow(2020).getBodyStyle()
         );
     }
 }
