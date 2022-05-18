c97a450196037fe7710c69f1299469ae4156ac50
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-18 15:44:16 +0800

    getModel() returns 0

--- src/main/java/com/mike/protobuf/VersionCompatibilityTest.java
+++ src/main/java/com/mike/protobuf/VersionCompatibilityTest.java
@@ -1,7 +1,6 @@
 package com.mike.protobuf;
 
 import com.mike.models.Television;
-import com.mike.models.Type;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,15 +13,14 @@ public class VersionCompatibilityTest {
         Path pathV2 = Paths.get("tv-v2");
         Path pathV3 = Paths.get("tv-v3");
 
-        Television television = Television.newBuilder().setBrand("Sony")
-                .setType(Type.OLED)
-                .build();
+//        Television television = Television.newBuilder().setBrand("Sony")
+//                .setType(Type.OLED)
+//                .build();
+//        Files.write(pathV3, television.toByteArray());
 
-        Files.write(pathV3, television.toByteArray());
-
-//        byte[] bytes = Files.readAllBytes(pathV2);
-//        System.out.println(
-//                Television.parseFrom(bytes)
-//        );
+        byte[] bytes = Files.readAllBytes(pathV3);
+        System.out.println(
+                Television.parseFrom(bytes).getModel()
+        );
     }
 }
