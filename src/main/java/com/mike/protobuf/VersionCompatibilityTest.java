c2f6404006f764042e2994f6434bc9b96cb0b49e
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-18 15:33:40 +0800

    use v1 model to parse v2 file

--- src/main/java/com/mike/protobuf/VersionCompatibilityTest.java
+++ src/main/java/com/mike/protobuf/VersionCompatibilityTest.java
@@ -1,7 +1,6 @@
 package com.mike.protobuf;
 
 import com.mike.models.Television;
-import com.mike.models.Type;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,18 +9,18 @@ import java.nio.file.Paths;
 
 public class VersionCompatibilityTest {
     public static void main(String[] args) throws IOException {
-        Television television = Television.newBuilder().setBrand("Sony")
-                .setModel(2020)
-                .setType(Type.OLED)
-                .build();
+//        Television television = Television.newBuilder().setBrand("Sony")
+//                .setModel(2020)
+//                .setType(Type.OLED)
+//                .build();
+//
+//        Path pathV2 = Paths.get("tv-v2");
+//        Files.write(pathV2, television.toByteArray());
 
         Path pathV2 = Paths.get("tv-v2");
-        Files.write(pathV2, television.toByteArray());
-
-//        Path pathV1 = Paths.get("tv-v1");
-//        byte[] bytes = Files.readAllBytes(pathV1);
-//        System.out.println(
-//                Television.parseFrom(bytes).getModel()
-//        );
+        byte[] bytes = Files.readAllBytes(pathV2);
+        System.out.println(
+                Television.parseFrom(bytes)
+        );
     }
 }
