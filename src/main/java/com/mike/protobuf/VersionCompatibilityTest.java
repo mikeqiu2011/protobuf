ae29db9892b5126ca617f9c7183bf734170df3ff
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-18 15:40:56 +0800

    generate v3 file

--- src/main/java/com/mike/protobuf/VersionCompatibilityTest.java
+++ src/main/java/com/mike/protobuf/VersionCompatibilityTest.java
@@ -1,6 +1,7 @@
 package com.mike.protobuf;
 
 import com.mike.models.Television;
+import com.mike.models.Type;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,17 +12,17 @@ public class VersionCompatibilityTest {
     public static void main(String[] args) throws IOException {
         Path pathV1 = Paths.get("tv-v1");
         Path pathV2 = Paths.get("tv-v2");
-//        Television television = Television.newBuilder().setBrand("Sony")
-//                .setModel(2020)
-//                .setType(Type.OLED)
-//                .build();
-//
-//        Path pathV2 = Paths.get("tv-v2");
-//        Files.write(pathV2, television.toByteArray());
+        Path pathV3 = Paths.get("tv-v3");
 
-        byte[] bytes = Files.readAllBytes(pathV1);
-        System.out.println(
-                Television.parseFrom(bytes)
-        );
+        Television television = Television.newBuilder().setBrand("Sony")
+                .setType(Type.OLED)
+                .build();
+
+        Files.write(pathV3, television.toByteArray());
+
+//        byte[] bytes = Files.readAllBytes(pathV2);
+//        System.out.println(
+//                Television.parseFrom(bytes)
+//        );
     }
 }
