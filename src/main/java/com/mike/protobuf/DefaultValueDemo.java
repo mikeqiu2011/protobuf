c03f14b9402fd964215e3053c5104e61b6a60de9
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-18 14:13:39 +0800

    restructure proto package and oneOf behaviour

---
+++ src/main/java/com/mike/protobuf/DefaultValueDemo.java
@@ -0,0 +1,13 @@
+package com.mike.protobuf;
+
+import com.mike.models.Person;
+
+public class DefaultValueDemo {
+    public static void main(String[] args) {
+        Person person = Person.newBuilder().build();
+        System.out.println(
+                "City : " + person.getAddress().getCity()
+        );
+        System.out.println(person.hasAddress());
+    }
+}
---
+++ src/main/java/com/mike/protobuf/OneOfDemo.java
@@ -0,0 +1,31 @@
+package com.mike.protobuf;
+
+import com.mike.models.Credentials;
+import com.mike.models.EmailCredentials;
+import com.mike.models.PhoneOTP;
+
+public class OneOfDemo {
+    public static void main(String[] args) {
+        EmailCredentials emailCredentials = EmailCredentials.newBuilder().setEmail("mike@gmail.com")
+                .setPassword("admin123")
+                .build();
+
+        PhoneOTP phoneOTP = PhoneOTP.newBuilder().setNumber(12345678)
+                .setCode(321)
+                .build();
+
+        Credentials credentials = Credentials.newBuilder()
+                .setEmailMode(emailCredentials)
+                .setPhoneMode(phoneOTP)
+                .build();
+
+        login(credentials);
+
+    }
+
+    private static void login(Credentials credentials) {
+        System.out.println(
+                credentials.getEmailMode()
+        );
+    }
+}
