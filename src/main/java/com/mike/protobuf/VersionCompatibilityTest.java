af0528e96e0cf0c1127fdf760901e52df75e864a
mikeqiu2011 <mikeqiu2011@163.com>
2022-05-18 15:22:38 +0800

    client use v1 to talk to us using v2

--- src/main/java/com/mike/protobuf/VersionCompatibilityTest.java
+++ src/main/java/com/mike/protobuf/VersionCompatibilityTest.java
@@ -9,15 +9,16 @@ import java.nio.file.Paths;
 
 public class VersionCompatibilityTest {
     public static void main(String[] args) throws IOException {
-        Television television = Television.newBuilder().setBrand("Sony")
-                .setYear(2019).build();
-
+//        Television television = Television.newBuilder().setBrand("Sony")
+//                .setYear(2019).build();
+//
+//        Path pathV1 = Paths.get("tv-v1");
+//        Files.write(pathV1, television.toByteArray());
+//        simulate client is also using v1 to talk to us
         Path pathV1 = Paths.get("tv-v1");
-        Files.write(pathV1, television.toByteArray());
-
         byte[] bytes = Files.readAllBytes(pathV1);
         System.out.println(
-                Television.parseFrom(bytes)
+                Television.parseFrom(bytes).getModel()
         );
     }
 }
